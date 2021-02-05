6abfbceb66e0d5136115d74cd58dd521d851524f
TomLloyd92 <c00231308@itcarlow.ie>
2021-02-05 12:30:08 +0000

    Initial Commit

---
+++ FuzzyLogic/FuzzyLogic/Gameplay.cpp
@@ -0,0 +1,25 @@
+#include "Gameplay.h"
+
+GamePlay::GamePlay()
+{
+}
+
+GamePlay::~GamePlay()
+{
+}
+
+void GamePlay::update(sf::Time t_deltaTime, sf::RenderWindow& t_window)
+{
+}
+
+void GamePlay::render(sf::RenderWindow& t_window)
+{
+}
+
+void GamePlay::setup(sf::Font& t_font)
+{
+}
+
+void GamePlay::initialise()
+{
+}
